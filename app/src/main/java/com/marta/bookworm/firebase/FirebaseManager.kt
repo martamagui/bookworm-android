3972d2a8ab27026765abb8243395c31c76586786
Marta Molina <marta.molina@live.u-tad.com>
2022-06-11 13:38:09 +0200

    Starting Firebase implementation

---
+++ app/src/main/java/com/marta/bookworm/firebase/FirebaseManager.kt
@@ -0,0 +1,16 @@
+package com.marta.bookworm.firebase
+
+import com.google.firebase.storage.FirebaseStorage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseManager {
+    @Provides
+    @Singleton
+    fun getFirebaseService(): FirebaseStorage
+}
