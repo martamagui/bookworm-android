044b7b5d1ddf784333e9efd04f70065e6ad5f3e6
Marta Molina <marta.molina@live.u-tad.com>
2022-06-14 20:27:25 +0200

    Added tagEvent

--- app/src/main/java/com/marta/bookworm/ui/createReview/step2/CreateReviewStep2Fragment.kt
+++ app/src/main/java/com/marta/bookworm/ui/createReview/step2/CreateReviewStep2Fragment.kt
@@ -1,12 +1,16 @@
 package com.marta.bookworm.ui.createReview.step2
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.google.android.material.chip.Chip
 import com.marta.bookworm.R
 import com.marta.bookworm.databinding.FragmentCreateReviewStep2Binding
 import com.marta.bookworm.ui.createReview.CreateReviewSharedViewModel
@@ -15,7 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class CreateReviewStep2Fragment : Fragment() {
     private var _binding: FragmentCreateReviewStep2Binding? = null
-    private val binding get () = _binding
+    private val binding get() = _binding!!
     private val viewModel: CreateReviewStep2ViewModel by viewModels()
     private val sharedViewModel: CreateReviewSharedViewModel by activityViewModels()
 
@@ -27,7 +31,58 @@ class CreateReviewStep2Fragment : Fragment() {
         return binding!!.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setEvents()
+    }
 
+
+    private fun createChip(tag: String) {
+        val chip = Chip(requireContext())
+        chip.text = "#$tag"
+        chip.chipBackgroundColor =
+            ColorStateList.valueOf(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.secondaryContainer
+                )
+            )
+        chip.chipStrokeColor =
+            ColorStateList.valueOf(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.secondaryContainer
+                )
+            )
+        chip.setOnCloseIconClickListener {
+            binding.cgTags.removeView(chip as View)
+            //TODO remove from shared the tag
+            //listTags.remove(text)
+        }
+        binding.cgTags.addView(chip as View)
+    }
+
+    private fun setEvents() {
+        tagEvent()
+        publishEvent()
+    }
+
+    private fun tagEvent() {
+        binding.etTags.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+                createChip(binding.etTags.text.toString())
+                binding.etTags.setText("")
+                return@OnKeyListener true
+            }
+            false
+        })
+    }
+
+    private fun publishEvent() {
+        binding.btnPublish.setOnClickListener {
+            //TODO publicar reseña
+        }
+    }
 
 
 }
